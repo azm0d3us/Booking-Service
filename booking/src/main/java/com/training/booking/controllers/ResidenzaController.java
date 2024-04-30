70a37cfe1b5835199de735730a6e8e633509744c
azm0d3us <lsergiodavide@gmail.com>
2024-04-30 07:08:21 +0200

    no message

--- booking/src/main/java/com/training/booking/controllers/ResidenzaController.java
+++ booking/src/main/java/com/training/booking/controllers/ResidenzaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/residenze")
 public class ResidenzaController {
